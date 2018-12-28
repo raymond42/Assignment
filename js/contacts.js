7b7d77a77f986bd184fd9b7464785cc4ca936f60
Raymond <raymond42.gr@gmail.com>
2018-12-28 02:39:51 +0200

    Alc js assignment1

--- js/contacts.js
+++ js/contacts.js
@@ -1,54 +1,39 @@
+let contacts = [];
+let quit = false;
+
 console.log("Welcome to your contacts manager!");
-function whatTodo(){
+
+while (!quit) 
+{
     console.log("1: List contacts");
     console.log("2: Add a contact");
-    console.log("0: Quit")
-    console.log("Here's the list of all your contacts");
-}
-do{
-whatTodo();
-var nmbr = prompt("Enter 1 to view your contact list, 2 to add new contact, or 0 to quit: ");
-function Names(last, first) {
-    this.last = last;
-    this.first = first;
-}
+    console.log("0: Quit");
+    let choice = prompt("Enter Choice");
 
-Names.prototype.describe = function() {
-    var description = "Last name: " + this.last + ", first name: " + this.first;
-        return description;
-}
- var person1 = new Names("Smith", "John");
- var person2 = new Names("Doe", "Jane");
- var persons = [];
- Names.push = (person1);
- Names.push = (person2);
-
- console.log(person1.describe());
- console.log(person2.describe());
-
-function addNewPerson (last, first){
-    this.last = last;
-    this.first = first;
-    var person3 = new Names(this.last , this.first);
-    Names.push = (person3);
-    console.log (person3.describe());
-}
-    var choice = parseInt(nmbr);
-    switch(choice)
+    if(choice == 0) 
     {
-        case 1 : 
-            console.log("Here`s the list of all your contacts: ");
-            for (var i=0; persons.length>i; i++)
-            {
-                console.log(persons[i]);
-            }
-            break;
-        case 2 : 
-            last = prompt("Enter last name for person: ");
-            first = prompt("Enter first name for person: ");
-            addNewPerson(last, first);
-            break;
-        default:
-            break;
+        quit = true;
     }
-} while (choice != 0);
+
+    if(choice == 1) 
+    {
+        console.log("Here's the list the of your contacts");
+        console.log("Last name: Smith, First name: John");
+        console.log("Last name: Doe, First name: Jane");
+
+        contacts.forEach(function(contact)
+        {
+            console.log("Last name:"+contact[1]+", First name:"+contact[0]);
+        });
+    }
+
+    if(choice == 2) 
+    {
+        let fname = prompt("Enter First Name")
+        let lname = prompt("Enter Last Name");
+        let contact = [fname, lname];
+        contacts.push(contact);
+        console.log("Contact added");
+        
+    }
+}
